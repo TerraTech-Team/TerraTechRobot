3bc0c51a8d4ebc85316daf60d8bfb185500d92df
arseniy1206 <arskad77@gmail.com>
2025-06-18 15:38:56 +0500

    Change resize logic

--- backend/ArtPixelation/GridResizer.cs
+++ backend/ArtPixelation/GridResizer.cs
@@ -9,26 +9,27 @@ public static class GridResizer
 {
     private static readonly IResampler resampler = KnownResamplers.Box;
 
-    public static Image<Rgba32> Resize(Image<Rgba32> original, int targetSize)
+    public static Image<Rgba32> Resize(Image<Rgba32> original, int quality)
     {
-        var padded = CreatePaddedSquareImage(original);
+        int newWidth, newHeight;
 
-        return padded.Clone(ctx => ctx.Resize(new ResizeOptions
+        if (original.Width >= original.Height)
         {
-            Size = new Size(targetSize, targetSize),
+            newWidth = quality;
+            newHeight = (int)Math.Round(original.Height * (quality / (double)original.Width));
+        }
+        else
+        {
+            newHeight = quality;
+            newWidth = (int)Math.Round(original.Width * (quality / (double)original.Height));
+        }
+
+        return original.Clone(ctx => ctx.Resize(new ResizeOptions
+        {
+            Size = new Size(newWidth, newHeight),
             Sampler = resampler,
             Mode = ResizeMode.Stretch
         }));
     }
-
-    private static Image<Rgba32> CreatePaddedSquareImage(Image<Rgba32> original)
-    {
-        var squareSize = Math.Max(original.Width, original.Height);
-        var padded = new Image<Rgba32>(squareSize, squareSize, new Rgba32(0, 0, 0, 0));
-        var offsetX = (squareSize - original.Width) / 2;
-        var offsetY = (squareSize - original.Height) / 2;
-
-        padded.Mutate(ctx => ctx.DrawImage(original, new Point(offsetX, offsetY), 1f));
-        return padded;
-    }
 }
+
